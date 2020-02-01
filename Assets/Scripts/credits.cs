6dcaf050702147804351f49f91abf91e94bf644e
Maxi <maximiliano.a.v.0@gmail.com>
2020-02-01 17:18:46 -0300

    creditos

---
+++ Assets/Scripts/credits.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class credits : MonoBehaviour
+{
+    public GameObject raw;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
