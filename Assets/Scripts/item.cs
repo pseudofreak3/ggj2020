27e4802710385ad6e21a3af53a3b4e1c51a1e391
Maxi <maximiliano.a.v.0@gmail.com>
2020-02-01 09:57:33 -0300

    Commit Initial

---
+++ Assets/Scripts/item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    public GameObject broken;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        broken.GetComponent<spawn>().cont++; 
+
+        Object.Destroy(this.gameObject);
+    }
+}
